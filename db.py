6e42a041451f4b0801c89e1d181d5135ae5a55ce
Martin Flink Ekblom <martin.ekblomflink99@gmail.com>
2025-12-09 18:20:02 +0100

    created some db-methods

--- db.py
+++ db.py
@@ -26,6 +26,47 @@ start with a connection parameter.
 #             items = cursor.fetchall()
 #     return items
 
+def get_users(con):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("SELECT * FROM users;")
+            users = cursor.fetchall()
+    return users
+
+def get_quizzes(con):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("SELECT * FROM quizzes;")
+            quizzes = cursor.fetchall()
+    return quizzes
+
+def get_all_session_players(con):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("SELECT * FROM session_players;")
+            session_players = cursor.fetchall()
+    return session_players
+
+def get_session_players(con, session_id):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("""SELECT * FROM session_players WHERE session_id = %s""", (session_id,))
+            session_players = cursor.fetchall()
+    return session_players
+
+def get_questions(con):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("SELECT * FROM questions;")
+            questions = cursor.fetchall()
+    return questions
+
+def get_quiz_questions(con, quiz_id):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("""SELECT * FROM questions WHERE quiz_id = %s""", (quiz_id,))
+            quiz_questions = cursor.fetchall()
+    return quiz_questions
 
 ### THIS IS JUST INSPIRATION FOR A DETAIL OPERATION (FETCHING ONE ENTRY)
 # def get_item(con, item_id):
@@ -34,6 +75,66 @@ start with a connection parameter.
 #             cursor.execute("""SELECT * FROM items WHERE id = %s""", (item_id,))
 #             item = cursor.fetchone()
 #             return item
+
+def get_user(con, user_id):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("""SELECT * FROM users WHERE id = %s""", (user_id,))             
+            user = cursor.fetchone()
+            return user
+        
+def get_quiz(con, quiz_id):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("""SELECT * FROM quizzes WHERE id = %s""", (quiz_id,))             
+            quiz = cursor.fetchone()
+            return quiz
+        
+def get_session_player(con, session_player_id):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("""SELECT * FROM session_players WHERE id = %s""", (session_player_id,))             
+            session_player = cursor.fetchone()
+            return session_player
+
+def get_question(con, question_id):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("""SELECT * FROM questions WHERE id = %s""", (question_id,))             
+            question = cursor.fetchone()
+            return question
+
+def get_question_answer_alternatives(con, question_id):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("""SELECT * FROM answer_alternatives WHERE question_id id = %s""", (question_id,))
+            answer_alternatives = cursor.fetchall()
+            return answer_alternatives
+
+def get_player_answer_for_question(con, player_id, question_id):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(
+                """SELECT * FROM player_answers 
+                    WHERE player_id = %s AND question_id = %s""",
+                    (player_id, question_id))
+            player_answer = cursor.fetchone()
+            return player_answer
+
+def get_scoreboard_for_session(con, session_id):
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("""SELECT * FROM session_scoreboard WHERE session_id = %s""", (session_id))
+            scoreboard = cursor.fetchone()
+            return scoreboard
+        
+# -------------------------------------------------------------------------------------------------------
+def get_item(con, table, item_id): # dålig säkerhet, måste fixas eller skitas i
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(f"""SELECT * FROM {table} WHERE id = %s""", (item_id,))             
+            item = cursor.fetchone()
+            return item
 
 
 ### THIS IS JUST INSPIRATION FOR A CREATE-OPERATION
@@ -46,3 +147,7 @@ start with a connection parameter.
 #             )
 #             item_id = cursor.fetchone()["id"]
 #     return item_id
+
+
+# STATISKA VÄRDEN KAN INSERTAS I PGADMIN
+# MER DYNAMISKA HÄR I METODERNA
